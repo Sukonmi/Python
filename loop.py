d22f3c03667dbc18ee5c0c0c2034a1808199533d
Sukonmi <danielayomideh@gmail.com>
2024-10-09 16:59:05 +0100

    I added a loop text file

---
+++ loop.py
@@ -0,0 +1,19 @@
+"""
+A loop is a code construct that runs a set of statements while a condition is true, and stops when the condition becomes false.
+There are two types of loops, which are:
+- While loop
+- For loop
+
+"""
+
+n = 1
+
+while n <=10:
+    print(f"n at {n} now")
+    n = n+1
+
+n = 5
+
+while n >=1:
+    print(f"n at {n} now")
+    n = n-1
