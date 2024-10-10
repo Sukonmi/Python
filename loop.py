ec13e025cabff5fb31c3c5e9d1d7977e8414f525
Sukonmi <danielayomideh@gmail.com>
2024-10-10 17:32:17 +0100

    I edited the loop text file

--- loop.py
+++ loop.py
@@ -26,3 +26,43 @@ while n <=20:
     print(f"{n} is an even number")
     n = n+2
 
+"""
+
+For loops are used to iterate(loop over) all elements in a 
+container (A container can be a list of values,
+a string of characters, range of numbers)
+one at a time. They are also used for counting.
+
+"""
+
+fruits = ["apple", "banana", "mango", "pinapple"]
+
+for f in fruits:
+    print(f.capitalize())
+
+numbers = [1,2,3,4,5,6,7,8]
+
+for n in numbers:
+    print(n*2)
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+for n in numbers:
+    print(f"3x{n} = {3*n}")
+
+for n in numbers:
+    remainder = n%2
+    if remainder == 1:
+        print(f"{n} is an odd number")
+
+students = {"name":"Ayomide", "age":20, "class":"ss3"}
+
+for s in students.keys():
+    print(s)
+
+for s in students.values():
+    print(s)
+
+for s in students.items():
+    print(s)
+    
