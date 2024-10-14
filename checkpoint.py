65d6b31fd55245ff149276b276e4c0b9b786f223
Sukonmi <danielayomideh@gmail.com>
2024-10-14 15:56:24 +0100

    I edited the code

--- checkpoint.py
+++ checkpoint.py
@@ -1,6 +1,7 @@
 shopping_list = []
 unique_items = set()
 items_info = {}
+our_store_items = {"Mango": 20, "Apple": 45, "Pineapple": 75, "Orange": 15, "Pear": 30}
 
 while True:
     print("""Shopping List Menu:\n
@@ -13,16 +14,21 @@ while True:
 
     if user_choice == 1:
         print("Add Item selected")
+        print("These are the items in our store currently")
+        for items in our_store_items:
+            print(item)
         for i in range(5):
-            item = input("Enter item: ")
-            if item in unique_items:
-                print(f"{item} is already in shopping list")
+            item = input("Enter item: \n").capitalize
+            if item in our_store_items:
+                if item in unique_items:
+                    print(f"{item} is already in shopping list")
             else:
-                item_quantity = int(input(f"How many quantity of {item}?"))
+                item_quantity = int(input(f"How many quantity of {item}? \n"))
                 shopping_list.append(item)
                 unique_items.add(item)
                 items_info[item] = item_quantity
                 print(f"{item} has been added to your shopping cart")
+    
     elif user_choice == 3:
         print("Displaying items in your cart")
         if len(shopping_list) > 0:
@@ -30,6 +36,7 @@ while True:
                 print(f"{item} with {items_info[item]} quantity")
         else:
             print("Your cart is empty")
+    
     elif user_choice == 4:
         print("Thank you for patronizing us")
         break
