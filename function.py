36121fe6b8745d64190f272a14c6c3dcabf68f39
Sukonmi <danielayomideh@gmail.com>
2024-10-15 17:23:20 +0100

    I edited a code file

--- function.py
+++ function.py
@@ -27,3 +27,24 @@ def area(b,h):
     return triangle_area
 
 print(f"Traingle area is {area(3,4)}")
+
+def minimum_number(a,b):
+    if a>b:
+        return b
+    else:
+        return a
+
+print(minimum_number(2,5))
+
+
+def print_scores(scores,bonus):
+    for score in scores:
+        print(f"{score} would be updated to {score+bonus}")
+
+print_scores([67,68,72,71,69],10)
+
+def multiplication_table(numbers,multiplier):
+    for n in numbers:
+        print(f"{multiplier}x{n} = {n*multiplier}")
+
+multiplication_table([1,2,3,4,5,6,7,8,9,10,11,12],1000)
