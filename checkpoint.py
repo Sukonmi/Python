4ac76f8aed7ea976c60ee755b333617e34eb5553
Sukonmi <danielayomideh@gmail.com>
2024-10-15 10:38:08 +0100

    I edited the code

--- checkpoint.py
+++ checkpoint.py
@@ -35,13 +35,13 @@ while True:
             if item in our_store_items:
                 if item in unique_items:
                     print(f"{item} is already in shopping list")
-                elif item not in unique_items:
+                else:
                     item_quantity = int(input(f"How many quantity of {item}?: "))
                     shopping_list.append(item)
-                unique_items.add(item)
-                items_info[item] = item_quantity
-                items_info[f"{item}_price"] = our_store_items[item] * item_quantity
-                print(f"{item} has been added to your shopping cart")
+                    unique_items.add(item)
+                    items_info[item] = item_quantity
+                    items_info[f"{item}_price"] = our_store_items[item] * item_quantity
+                    print(f"{item} has been added to your shopping cart")
             else:
                 print("Sorry! We do not have this currently.")
 
@@ -49,11 +49,13 @@ while True:
         print("Displaying items in your cart")
         if len(shopping_list) > 0:
             for item in shopping_list:
-                print(f"{item} with {items_info[item]} quanity(s) with price of {items_info[f"{item}_price"]} Naira")
+                print(f"{item} with {items_info[item]} quanity(s) with price of {items_info[f"{item}_price"]}")
         else:
             print("Your cart is empty")
     
     elif user_choice == 4:
         print("Thank you for patronizing us")
         break
+
+
 
