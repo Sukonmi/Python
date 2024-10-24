02fcba54ecc54eae46876fab1bd1ce91951201df
Sukonmi <danielayomideh@gmail.com>
2024-10-24 16:36:53 +0100

    I edited the folder

--- filehandling.py
+++ filehandling.py
@@ -1,9 +1,16 @@
+import numpy as np
+
 with open("text.txt", "a") as file_object:
     loans = file_object.readlines()
 
 all_loan_amounts = []
 for row in loans:
     loan_amount = row.split(",")[8]
+    if loan_amount == "":
+        continue
+    all_loan_amounts.append(int(loan_amount))
     all_loan_amounts = loan_amount
 
 print(all_loan_amounts)
+
+loan_array = np.array(all_loan_amounts)
